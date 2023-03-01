521c6c870f6de593b4f0e3e3a3df15ee8c991bf2
Thảo GiBi <125585308+ThaoGiBiBehemoth@users.noreply.github.com>
2023-03-01 15:27:09 +0700

    Chap 4th done

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,15 @@
+class StaticPagesController < ApplicationController
+  # layout false
+
+  def home
+  end
+
+  def help
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  
+  # root 'application#hello'
+  root 'static_pages#home'
+
+  get '/home', to: 'static_pages#home' 
+  get '/help', to: 'static_pages#help' #, as: 'helf' (khi đó truy cập /help -> chính là helf_path hay vì help_path)
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  
+  get 'signup', to: 'users#new'
+  get 'new', to: 'users#new'
+  
+end
